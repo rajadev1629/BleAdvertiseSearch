1601c0f4250a3aec4b927e168d9f1c9d11b8776e
Raja <Raja@retechlabs.com>
2020-08-19 14:57:11 +0530

    Add foreground Service

---
+++ app/src/main/java/com/demo/bletest/BleService.java
@@ -0,0 +1,48 @@
+package com.demo.bletest;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class BleService extends Service {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = "Ble Service";
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+
+        Notification notification = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                .setContentTitle(input)
+                .setContentText(input + " Running")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- app/src/main/java/com/demo/bletest/MainActivity.java
+++ app/src/main/java/com/demo/bletest/MainActivity.java
@@ -1,38 +1,39 @@
 package com.demo.bletest;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
-import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.AdvertisingSet;
+import android.bluetooth.le.AdvertisingSetCallback;
+import android.bluetooth.le.AdvertisingSetParameters;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Handler;
 import android.os.ParcelUuid;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -43,20 +44,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BluetoothLeScanner mBluetoothLeScanner;
     private Handler mHandler = new Handler();
 
+    private static final int PERMISSION_REQ_ID = 21;
+    private static final String[] REQUESTED_PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN
+    };
+
+    StringBuilder builder = new StringBuilder();
+    long time1;
+    public static final String LOG_TAG = "kkkkkkkkk";
+    public static final String CHANNEL_ID = "BLE_CHANNEL";
+
+
+
+
+
     private ScanCallback mScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-            if( result == null
+            if (result == null
                     || result.getDevice() == null
-                    || TextUtils.isEmpty(result.getDevice().getName()) )
+                    || TextUtils.isEmpty(result.getDevice().getAddress()))
                 return;
 
-            StringBuilder builder = new StringBuilder( result.getDevice().getName() );
-
-            builder.append("\n").append(new String(result.getScanRecord().getServiceData(result.getScanRecord().getServiceUuids().get(0)), Charset.forName("UTF-8")));
-
+            builder.append("\n" + result.getDevice().getAddress() + result.getScanRecord().getDeviceName());
             mText.setText(builder.toString());
+            long time = System.currentTimeMillis();
+
+            if ((time - time1) > 5000) {
+                Toast.makeText(MainActivity.this, result.getDevice().getAddress() + result.getDevice().getName(), Toast.LENGTH_LONG).show();
+                time1 = time;
+            }
         }
 
         @Override
@@ -66,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onScanFailed(int errorCode) {
-            Log.e( "BLE", "Discovery onScanFailed: " + errorCode );
+            Log.e("BLE", "Discovery onScanFailed: " + errorCode);
             super.onScanFailed(errorCode);
         }
     };
@@ -76,63 +97,116 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ble);
 
-        mText = (TextView) findViewById( R.id.text );
-        mDiscoverButton = (Button) findViewById( R.id.discover_btn );
-        mAdvertiseButton = (Button) findViewById( R.id.advertise_btn );
+        mText = (TextView) findViewById(R.id.text);
+        mDiscoverButton = (Button) findViewById(R.id.discover_btn);
+        mAdvertiseButton = (Button) findViewById(R.id.advertise_btn);
 
-        mDiscoverButton.setOnClickListener( this );
-        mAdvertiseButton.setOnClickListener( this );
+        mDiscoverButton.setOnClickListener(this);
+        mAdvertiseButton.setOnClickListener(this);
 
-        mBluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mText.setText(mBluetoothAdapter.getAddress() + " : " + mBluetoothAdapter.getRemoteDevice(mBluetoothAdapter.getAddress()).getName());
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "No bluetooth support on this device", Toast.LENGTH_SHORT).show();
 
-        if( !BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
-            Toast.makeText( this, "Multiple advertisement not supported", Toast.LENGTH_SHORT ).show();
-            mAdvertiseButton.setEnabled( false );
-            mDiscoverButton.setEnabled( false );
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            // Bluetooth is not enabled :)
+            mBluetoothAdapter.enable();
+
+
+        }
+
+        ActivityCompat.requestPermissions(MainActivity.this, REQUESTED_PERMISSIONS, 101);
+
+        createNotificationChannel();
+        startService();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 101: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contact-related task you need to do.
+                    init();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied cant continue", Toast.LENGTH_SHORT).show();
+                    System.exit(0);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 
+    private void init() {
+//        if (!BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported()) {
+//            Toast.makeText(this, "Multiple advertisement not supported", Toast.LENGTH_SHORT).show();
+//            mAdvertiseButton.setEnabled(false);
+//            mDiscoverButton.setEnabled(false);
+//        }
+    }
+
     private void discover() {
+        time1 = System.currentTimeMillis();
         List<ScanFilter> filters = new ArrayList<ScanFilter>();
 
         ScanFilter filter = new ScanFilter.Builder()
-                .setServiceUuid( new ParcelUuid(UUID.fromString( getString(R.string.ble_uuid ) ) ) )
+                .setServiceUuid(new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid))))
                 .build();
-        filters.add( filter );
+        filters.add(filter);
 
         ScanSettings settings = new ScanSettings.Builder()
-                .setScanMode( ScanSettings.SCAN_MODE_LOW_LATENCY )
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
                 .build();
 
         mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
 
-        mHandler.postDelayed(new Runnable() {
+       /* mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 mBluetoothLeScanner.stopScan(mScanCallback);
             }
-        }, 10000);
+        }, 10000);*/
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void advertise() {
         BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
 
-        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+        /*AdvertiseSettings settings = new AdvertiseSettings.Builder()
                 .setAdvertiseMode( AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY )
                 .setTxPowerLevel( AdvertiseSettings.ADVERTISE_TX_POWER_HIGH )
                 .setConnectable(false)
                 .setTimeout(10000)
+                .build();*/
+
+        AdvertisingSetParameters parameters = (new AdvertisingSetParameters.Builder())
+                .setLegacyMode(true) // True by default, but set here as a reminder.
+                .setConnectable(false)
+                .setScannable(true)
+                .setInterval(AdvertisingSetParameters.INTERVAL_LOW)
+                .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_HIGH)
                 .build();
 
-        ParcelUuid pUuid = new ParcelUuid( UUID.fromString( getString( R.string.ble_uuid ) ) );
+        ParcelUuid pUuid = new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid)));
 
         AdvertiseData data = new AdvertiseData.Builder()
-                .setIncludeDeviceName( true )
-                .addServiceUuid( pUuid )
-                .addServiceData( pUuid, "Data".getBytes(Charset.forName("UTF-8") ) )
+                .setIncludeDeviceName(false)
+                .addServiceUuid(pUuid)
                 .build();
 
-        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
+        /*AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
             @Override
             public void onStartSuccess(AdvertiseSettings settingsInEffect) {
                 super.onStartSuccess(settingsInEffect);
@@ -143,17 +217,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.e( "BLE", "Advertising onStartFailure: " + errorCode );
                 super.onStartFailure(errorCode);
             }
-        };
+        };*/
 
-        advertiser.startAdvertising( settings, data, advertisingCallback );
+
+        AdvertisingSetCallback callback = new AdvertisingSetCallback() {
+            @Override
+            public void onAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, int status) {
+                Log.i(LOG_TAG, "onAdvertisingSetStarted(): txPower:" + txPower + " , status: "
+                        + status);
+            }
+
+            @Override
+            public void onAdvertisingDataSet(AdvertisingSet advertisingSet, int status) {
+                Log.i(LOG_TAG, "onAdvertisingDataSet() :status:" + status);
+            }
+
+            @Override
+            public void onScanResponseDataSet(AdvertisingSet advertisingSet, int status) {
+                Log.i(LOG_TAG, "onScanResponseDataSet(): status:" + status);
+            }
+
+            @Override
+            public void onAdvertisingSetStopped(AdvertisingSet advertisingSet) {
+                Log.i(LOG_TAG, "onAdvertisingSetStopped():");
+            }
+        };
+        advertiser.startAdvertisingSet(parameters, data, null, null, null, callback);
+
+
+       /* if (currentAdvertisingSet != null) {
+            // After onAdvertisingSetStarted callback is called, you can modify the
+            // advertising data and scan response data:
+            currentAdvertisingSet.setAdvertisingData(new AdvertiseData.Builder().
+                    setIncludeDeviceName(true).setIncludeTxPowerLevel(true).build());
+            // Wait for onAdvertisingDataSet callback...
+            currentAdvertisingSet.setScanResponseData(new
+                    AdvertiseData.Builder().addServiceUuid(new ParcelUuid(UUID.randomUUID())).build());
+            // Wait for onScanResponseDataSet callback...
+
+            // When done with the advertising:
+            advertiser.stopAdvertisingSet(callback);
+        }*/
+
+
+        /* advertiser.startAdvertising( settings, data, advertisingCallback );*/
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View v) {
-        if( v.getId() == R.id.discover_btn ) {
+        mBluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+
+        if (v.getId() == R.id.discover_btn) {
             discover();
-        } else if( v.getId() == R.id.advertise_btn ) {
+        } else if (v.getId() == R.id.advertise_btn) {
             advertise();
         }
     }
+
+    private void startService() {
+        Intent intentService = new Intent(MainActivity.this, BleService.class);
+        intentService.putExtra("putExtra", "Ble Service");
+        startService(intentService);
+    }
+
+    private void stopService() {
+        Intent intentService = new Intent(MainActivity.this, BleService.class);
+        stopService(intentService);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    "Example Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(notificationChannel);
+        };
+
+    }
+
 }
